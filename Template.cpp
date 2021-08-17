36e9ef143d9169c781e91da5a668393509239fa8
Trung Hải <CBG.hai.09@gmail.com>
2021-08-18 02:42:54 +0700

    add geometry fix some bug

--- Template.cpp
+++ Template.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 //=== euclidean extended ===
 //{
-    void Diophante(long long a, long long b, long long &d, long long&x)
+    void diophante(long long a, long long b, long long &d, long long&x)
     {
       long long m, n, r, q, xm, xn, xr;
       m = a; xm = 1;
@@ -52,7 +52,7 @@ using namespace std;
             }
         }
     };
-    long long Mul(long long x, long long y,long long m) /// Tinh (x * y) mod M voi (x,y, M <= 10 ^ 18)
+    long long mul(long long x, long long y,long long m) /// Tinh (x * y) mod M voi (x,y, M <= 10 ^ 18)
     {
         x %= m; y %= m;
         long long q = (long double) x * y / m;
@@ -65,7 +65,8 @@ using namespace std;
 
 
 //=== prime ===
-    vector <int> Eratosthenes(int limit)
+//{
+    vector <int> eratosthenes(int limit)
     {
         vector <int> isPrime(limit + 1, 0);
 
@@ -84,7 +85,140 @@ using namespace std;
         return isPrime;
     }
 
+    vector <int> primeArray(int limit)
+    {
+        vector <int> primeArray;
+        vector <int> isPrime = eratosthenes(limit);
+        for (int i=1; i<=limit; i++)
+            if (isPrime[i] == 0)
+                primeArray.push_back(i);
+        return primeArray;
+    }
+//}
 
+//=== geometry ===
+//{
+    ///minimum enclosing circle O(N)
+    ///https://www.geeksforgeeks.org/minimum-enclosing-circle-set-2-welzls-algorithm/
+    struct Point {
+        double x, y;
+        Point(){};
+        Point(double _x, double _y) : x(_x), y(_y) {};
+        Point(const Point &A) : x(A.x), y(A.y) {};
+        int operator < (const Point &A) {
+            return make_pair(x, y) < make_pair(A.x, A.y);
+        }
+        int operator == (const Point &A) {
+            return make_pair(x, y) == make_pair(A.x, A.y);
+        }
+    };
+
+    struct Circle {
+        Point center;
+        double r;
+        Circle(){};
+        Circle(double _x, double _y, double _r) : center(Point(_x, _y)), r(_r) {};
+        Circle(Point _center, double _r) : center(_center), r(_r) {};
+        Circle(const Circle &A) : center(A.center), r(A.r) {};
+        int operator < (const Circle &A) {
+            return r < A.r;
+        }
+    };
+
+    double dist(Point A, Point B) {
+        double x = A.x - B.x;
+        double y = A.y - B.y;
+        return sqrtl(x * x + y * y);
+    }
+
+    bool inSide(Point A, Circle C) {
+        return dist(A, C.center) <= C.r;
+    }
+
+    Point get_circle_center(double bx, double by,
+                            double cx, double cy)
+    {
+        double B = bx * bx + by * by;
+        double C = cx * cx + cy * cy;
+        double D = bx * cy - by * cx;
+        return { (cy * B - by * C) / (2 * D),
+                 (bx * C - cx * B) / (2 * D) };
+    }
+
+    Circle circle_three(Point A, Point B, Point C) {
+        Point O = get_circle_center(B.x - A.x, B.y - A.y, C.x - A.x, C.y - A.y);
+        O.x += A.x;
+        O.y += A.y;
+        return Circle(O, dist(O, A));
+    }
+
+    Circle circle_two(Point A, Point B) {
+        Point O = Point((A.x + B.x) / 2.0, (A.y + B.y) / 2.0);
+        return Circle(O, dist(A, B) / 2.0);
+    }
+
+    bool check_valid_circle(Circle C, vector<Point> p) {
+        for (Point A : p) {
+            if (!inSide(A, C)) return false;
+        }
+        return true;
+    }
+
+    // Function to return the minimum enclosing circle for N <= 3
+    Circle MEC_small(vector<Point> p) {
+        assert((int)p.size() <= 3);
+        if (p.empty()) return Circle(Point(0, 0), 0);
+        if ((int)p.size() == 1) return Circle(p[0], 0);
+        if ((int)p.size() == 2) return circle_two(p[0], p[1]);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 3; j++) {
+                Circle C = circle_two(p[i], p[j]);
+                if (check_valid_circle(C, p)) return C;
+            }
+        }
+        Circle res = circle_three(p[0], p[1], p[2]);
+        return res;
+    }
+
+    Circle welzl_helper(vector<Point>& P, vector<Point> R, int n)
+    {
+        if (n == 0 || R.size() == 3) {
+            return MEC_small(R);
+        }
+
+        int idx = rng() % n;
+        Point p = P[idx];
+
+        swap(P[idx], P[n - 1]);
+
+        Circle C = welzl_helper(P, R, n - 1);
+
+        if (inSide(p, C)) {
+            return C;
+        }
+
+        R.push_back(p);
+
+        return welzl_helper(P, R, n - 1);
+    }
+
+    ///minimum enclosing circle O(N)
+    Circle MEC(vector<Point> p) {
+        int n = (int)p.size();
+        random_shuffle(p.begin(), p.end());
+        return welzl_helper(p, {}, n);
+    }
+
+    ///minimum enclosing circle O(N)
+    Circle get(vector<double> &x, vector<double> &y) {
+        int n = (int)x.size();
+        vector<Point> p(n);
+        for (int i = 0; i < n; i++) p[i] = Point(x[i], y[i]);
+        return MEC(p);
+    }
+
+//}
 int main()
 {
 
